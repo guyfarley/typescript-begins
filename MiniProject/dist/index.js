3ab1588d1d83e0dd4bfe85fc57544d3483516afc
Guy Farley <guy.e.farley@gmail.com>
2022-10-26 11:01:33 -0700

    completed mini project

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -2,12 +2,40 @@
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
 const form = document.querySelector("form");
+const list = document.getElementById("todolist");
+const todos = readTodos();
+todos.forEach(createTodo);
+function readTodos() {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
+}
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
 function handleSubmit(e) {
     e.preventDefault();
-    console.log("SUBMITTED");
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    saveTodos();
+    input.value = "";
+}
+function createTodo(todo) {
+    const newLI = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", function () {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    newLI.append(checkbox);
+    newLI.append(todo.text);
+    list.append(newLI);
 }
 form.addEventListener("submit", handleSubmit);
-// btn?.addEventListener("click", function () {
-//   alert(input.value);
-//   input.value = "";
-// });
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,17 +1,53 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
+const list = document.getElementById("todolist")!;
+
+interface Todo {
+  text: string,
+  completed: boolean,
+}
+
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
+
+function readTodos(): Todo[] {
+  const todosJSON = localStorage.getItem("todos");
+  if (todosJSON === null) return [];
+  return JSON.parse(todosJSON);
+}
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  console.log("SUBMITTED");
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  }
+  createTodo(newTodo);
+  todos.push(newTodo);
+  saveTodos();
+  input.value = "";
+}
+
+function createTodo(todo: Todo) {
+  const newLI = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = todo.completed;
+
+  checkbox.addEventListener("change", function () {
+    todo.completed = checkbox.checked;
+    saveTodos();
+  });
+
+  newLI.append(checkbox);
+  newLI.append(todo.text);
+  list.append(newLI);
 }
 
 form.addEventListener("submit", handleSubmit);
-
-
-// btn?.addEventListener("click", function () {
-//   alert(input.value);
-//   input.value = "";
-// });
 

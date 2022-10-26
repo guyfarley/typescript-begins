d1ae3dfee5fca2c4f60b3aa907f44d35338b7a58
Guy Farley <guy.e.farley@gmail.com>
2022-10-26 10:24:38 -0700

    handleSubmit

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -1,3 +1,13 @@
 "use strict";
 const btn = document.getElementById("btn");
-console.log(btn);
+const input = document.getElementById("todoinput");
+const form = document.querySelector("form");
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log("SUBMITTED");
+}
+form.addEventListener("submit", handleSubmit);
+// btn?.addEventListener("click", function () {
+//   alert(input.value);
+//   input.value = "";
+// });
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,2 +1,17 @@
-const btn = document.getElementById("btn");
-console.log(btn);
+const btn = document.getElementById("btn")! as HTMLButtonElement;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
+
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  console.log("SUBMITTED");
+}
+
+form.addEventListener("submit", handleSubmit);
+
+
+// btn?.addEventListener("click", function () {
+//   alert(input.value);
+//   input.value = "";
+// });
+

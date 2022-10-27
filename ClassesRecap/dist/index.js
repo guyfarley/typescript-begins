7b4eb86771fe00f086f7915f849a0dab21fca944
Guy Farley <guy.e.farley@gmail.com>
2022-10-27 12:21:27 -0700

    Typescript Classes

--- ClassesRecap/dist/index.js
+++ ClassesRecap/dist/index.js
@@ -1,8 +1,96 @@
 "use strict";
 class Player {
+    constructor(first, last, _score) {
+        this.first = first;
+        this.last = last;
+        this._score = _score;
+    }
+    secretMethod() {
+        console.log("SECRET METHOD!");
+    }
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0) {
+            throw new Error("Score cannot be negative");
+        }
+        this._score = newScore;
+    }
+}
+class SuperPlayer extends Player {
+    constructor() {
+        super(...arguments);
+        this.isAdmin = true;
+    }
+    maxScore() {
+        this._score = 99999999;
+    }
+}
+let lassen = new Player("Lassen", "Farley", 100);
+lassen.fullName;
+lassen.score = 90;
+class Bike {
+    constructor(color) {
+        this.color = color;
+    }
+}
+class Jacket {
+    constructor(brand, color) {
+        this.brand = brand;
+        this.color = color;
+    }
+    print() {
+        console.log(`${this.color} ${this.brand}`);
+    }
+}
+const bike = new Bike("red");
+const jacket = new Jacket("Prada", "black");
+class Employee {
     constructor(first, last) {
         this.first = first;
         this.last = last;
     }
+    greet() {
+        console.log("HELLO!");
+    }
 }
-let lassen = new Player("Lassen", "Farley");
+class FullTimeEmployee extends Employee {
+    constructor(first, last, salary) {
+        super(first, last);
+        this.salary = salary;
+    }
+    getPay() {
+        return this.salary;
+    }
+}
+class PartTimeEmployee extends Employee {
+    constructor(first, last, hourlyRate, hoursWorked) {
+        super(first, last);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+    getPay() {
+        return this.hourlyRate * this.hoursWorked;
+    }
+}
+const betty = new FullTimeEmployee("Betty", "White", 95000);
+console.log(betty.getPay());
+const bill = new PartTimeEmployee("Bill", "Billerson", 24, 1100);
+console.log(bill.getPay());
+// class Player {
+//   public readonly first: string;
+//   public readonly last: string;
+//   private score = 0; // or score: number =0;
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//     this.secretMethod;
+//   }
+//   private secretMethod(): void {
+//     console.log("SECRET METHOD!");
+//   }
+// }
--- ClassesRecap/src/index.ts
+++ ClassesRecap/src/index.ts
@@ -1,10 +1,108 @@
 class Player {
-  first: string;
-  last: string;
-  constructor(first: string, last: string) {
-    this.first = first;
-    this.last = last;
+
+  constructor(public first: string, public last: string, protected _score: number) { }
+
+  private secretMethod(): void {
+    console.log("SECRET METHOD!");
+  }
+
+  get fullName(): string {
+    return `${this.first} ${this.last}`;
+  }
+
+  get score(): number {
+    return this._score;
+  }
+
+  set score(newScore: number) {
+    if (newScore < 0) {
+      throw new Error("Score cannot be negative");
+    }
+    this._score = newScore;
   }
 }
 
-let lassen = new Player("Lassen", "Farley");
+class SuperPlayer extends Player {
+  public isAdmin: boolean = true;
+  maxScore() {
+    this._score = 99999999;
+  }
+}
+
+let lassen = new Player("Lassen", "Farley", 100);
+lassen.fullName;
+lassen.score = 90;
+
+interface Colorful {
+  color: string;
+}
+
+interface Printable {
+  print(): void;
+}
+
+class Bike implements Colorful {
+  constructor(public color: string) { }
+}
+
+class Jacket implements Colorful, Printable {
+  constructor(public brand: string, public color: string) { }
+
+  print(): void {
+    console.log(`${this.color} ${this.brand}`)
+  }
+}
+
+const bike = new Bike("red");
+const jacket = new Jacket("Prada", "black");
+
+
+abstract class Employee {
+  constructor(public first: string, public last: string) { }
+  abstract getPay(): number;
+  greet() {
+    console.log("HELLO!")
+  }
+}
+
+class FullTimeEmployee extends Employee {
+  constructor(first: string, last: string, private salary: number) {
+    super(first, last);
+  }
+  getPay() {
+    return this.salary;
+  }
+}
+
+class PartTimeEmployee extends Employee {
+  constructor(first: string, last: string, private hourlyRate: number, private hoursWorked: number) {
+    super(first, last);
+  }
+
+  getPay() {
+    return this.hourlyRate * this.hoursWorked;
+  }
+}
+
+const betty = new FullTimeEmployee("Betty", "White", 95000);
+console.log(betty.getPay());
+
+const bill = new PartTimeEmployee("Bill", "Billerson", 24, 1100);
+console.log(bill.getPay());
+
+
+// class Player {
+//   public readonly first: string;
+//   public readonly last: string;
+//   private score = 0; // or score: number =0;
+
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//     this.secretMethod;
+//   }
+
+//   private secretMethod(): void {
+//     console.log("SECRET METHOD!");
+//   }
+// }
